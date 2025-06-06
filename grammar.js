96feccd42f915aefc559627da8c6dba77e07a535
andreasjansson <andreas@replicate.ai>
2025-06-06 20:53:49 +0200

    Remove conflicts - not needed with simpler approach

--- grammar.js
+++ grammar.js
@@ -21,8 +21,7 @@ module.exports = grammar({
   ],
 
   conflicts: $ => [
-    [$.content, $.content_with_citations],
-    [$.content, $.content_with_citations, $.line_with_cite_tags],
+    // Add conflicts here if needed
   ],
 
   rules: {
