ad488457fc3c33bf81098901e0c632a3aeecb432
andreasjansson <andreas@replicate.ai>
2025-06-06 21:04:45 +0200

    Add conflict for content vs citations_with_text

--- grammar.js
+++ grammar.js
@@ -24,6 +24,7 @@ module.exports = grammar({
     [$.citations_with_text, $.citations_without_text],
     [$.assistant_section, $.assistant_section_with_citations],
     [$.content, $.content_with_citations],
+    [$.content, $.citations_with_text],
   ],
 
   rules: {
