eef26f1ac117c1953c95f5e98ec343dfc4983dac
andreasjansson <andreas@replicate.ai>
2025-06-06 17:28:14 +0200

    Fix citation entry parsing conflict with left precedence

--- grammar.js
+++ grammar.js
@@ -236,7 +236,7 @@ module.exports = grammar({
     ),
 
     // Citation patterns
-    citation_entry: $ => seq(
+    citation_entry: $ => prec.left(seq(
       "###",
       /[ \t]*/,
       field("url", $.url),
@@ -248,7 +248,7 @@ module.exports = grammar({
         $.citation_index,
         $.newline
       ))
-    ),
+    )),
 
     citation_title: $ => seq(
       "Title:",
