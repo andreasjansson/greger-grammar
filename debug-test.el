525628ac7105133a073bff9adca5bb759a4eacd5
andreasjansson <andreas@replicate.ai>
2025-06-07 08:30:45 +0200

    Fix debug script to use proper temp buffer parsing

--- debug-test.el
+++ debug-test.el
@@ -3,19 +3,19 @@
 (load-file "./greger-tree-sitter.el")
 
 ;; Debug simple tree structure first
-(let* ((text "## USER:
+(let ((text "## USER:
 
-Hello, how are you?")
-       (parser (treesit-parser-create 'greger))
-       (root-node (progn (with-temp-buffer
-                          (insert text)
-                          (treesit-parser-root-node parser)))))
+Hello, how are you?"))
+  (with-temp-buffer
+    (insert text)
+    (let* ((parser (treesit-parser-create 'greger))
+           (root-node (treesit-parser-root-node parser)))
 
-  (message "Root node type: %s" (treesit-node-type root-node))
-  (message "Root node children: %d" (treesit-node-child-count root-node))
-  (dotimes (i (treesit-node-child-count root-node))
-    (let ((child (treesit-node-child root-node i)))
-      (message "  Root child %d: %s" i (treesit-node-type child))))
+      (message "Root node type: %s" (treesit-node-type root-node))
+      (message "Root node children: %d" (treesit-node-child-count root-node))
+      (dotimes (i (treesit-node-child-count root-node))
+        (let ((child (treesit-node-child root-node i)))
+          (message "  Root child %d: %s" i (treesit-node-type child))))
 
-  (let ((sections (greger-tree-sitter--get-all-sections root-node)))
-    (message "Sections found by get-all-sections: %d" (length sections))))
+      (let ((sections (greger-tree-sitter--get-all-sections root-node)))
+        (message "Sections found by get-all-sections: %d" (length sections))))))
