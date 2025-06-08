4770e74feca5bff21b3ab577877a715d1b9c5f95
andreasjansson <andreas@replicate.ai>
2025-06-08 12:40:00 +0200

    Update simple-user-message test case to read from corpus file

--- test-all-greger-cases.el
+++ test-all-greger-cases.el
@@ -17,9 +17,7 @@
   '(
     ;; Simple user message
     (:name "simple-user-message"
-           :markdown "## USER:
-
-Hello, how are you?"
+           :markdown (greger-read-corpus-file "simple-user-message")
            :dialog (((role . "user") (content . "Hello, how are you?"))))
 
     ;; System and user message
