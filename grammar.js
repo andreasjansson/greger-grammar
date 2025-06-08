9e4a6f80d0a806d714a2d87efc9a231645b994b0
andreasjansson <andreas@replicate.ai>
2025-06-09 00:25:39 +0200

    Add separate tool_result_content rule

--- grammar.js
+++ grammar.js
@@ -84,9 +84,13 @@ module.exports = grammar({
       ':',
       /\n/,
       optional(/\n/),
-      repeat($.id),
-      alias($.tool_content, 'content'),
+      repeat(choice(
+        $.id,
+        $.tool_result_content,
+      )),
     ),
+
+    tool_result_content: $ => alias($.tool_content, 'content'),
 
     server_tool_use: $ => seq(
       '##',
