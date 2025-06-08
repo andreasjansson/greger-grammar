b5f653ae3b453f4d7ef1b5568361312c119ca4de
andreasjansson <andreas@replicate.ai>
2025-06-08 23:06:59 +0200

    Remove token wrapper from tool param name to expose content

--- grammar.js
+++ grammar.js
@@ -150,7 +150,7 @@ module.exports = grammar({
 
     tool_param: $ => seq(
       '###',
-      field('name', token(/[ ]*[^\n]*/)),
+      field('name', /[ ]*[^\n]*/),
       /\n+/,
       field('value', $.tool_content),
     ),
