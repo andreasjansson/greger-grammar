c6c7ec58e6d8fc121530aeb640a8b41be6019017
andreasjansson <andreas@replicate.ai>
2025-06-08 16:05:20 +0200

    Update debug test to use tool-use-with-code-in-params test case

--- debug-test.el
+++ debug-test.el
@@ -1,31 +1,8 @@
 (load-file "./greger-tree-sitter.el")
 
-(let* ((text "## USER:
-
-Search for python files containing 'def main'
-
-## TOOL USE:
-
-Name: ripgrep
-ID: toolu_456
-
-### pattern
-
-<tool.toolu_456>
-def main
-</tool.toolu_456>
-
-### file-type
-
-<tool.toolu_456>
-py
-</tool.toolu_456>
-
-### context-lines
-
-<tool.toolu_456>
-2
-</tool.toolu_456>")
+(let* ((text (with-temp-buffer
+               (insert-file-contents "./test/corpus/tool-use-with-code-in-params.greger")
+               (buffer-string)))
        (result (greger-tree-sitter-parse text)))
   (message "Result:")
   (pp result))
