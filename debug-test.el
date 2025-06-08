37240319421713014ed280590bd0c73900419b6f
andreasjansson <andreas@replicate.ai>
2025-06-08 16:22:53 +0200

    Update debug test to check server-tool-use-basic

--- debug-test.el
+++ debug-test.el
@@ -1,7 +1,7 @@
 (load-file "./greger-tree-sitter.el")
 
 (let* ((text (with-temp-buffer
-               (insert-file-contents "./test/corpus/citations-after-tool-result.greger")
+               (insert-file-contents "./test/corpus/server-tool-use-basic.greger")
                (buffer-string)))
        (result (greger-tree-sitter-parse text)))
   (message "Result:")
