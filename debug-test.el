846a13b1f7d744decce0512509b04b4cc300dbbb
andreasjansson <andreas@replicate.ai>
2025-06-08 16:06:22 +0200

    Update debug test to check quote unescaping

--- debug-test.el
+++ debug-test.el
@@ -1,7 +1,7 @@
 (load-file "./greger-tree-sitter.el")
 
 (let* ((text (with-temp-buffer
-               (insert-file-contents "./test/corpus/tool-use-with-code-in-params.greger")
+               (insert-file-contents "./test/corpus/multiple-tool-uses.greger")
                (buffer-string)))
        (result (greger-tree-sitter-parse text)))
   (message "Result:")
