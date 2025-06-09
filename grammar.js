a706743c5aadf10a2c644487eb627c45a1643d85
andreasjansson <andreas@replicate.ai>
2025-06-09 13:11:07 +0200

    Use field on tool_content directly in tool_param

--- grammar.js
+++ grammar.js
@@ -156,7 +156,7 @@ module.exports = grammar({
       alias($.param_name, $.name),
       /\n/,
       optional(/\n/),
-      alias($._tool_tag, $.value),
+      field('value', alias($.tool_content, $.value_content)),
     ),
 
     param_name: $ => /[^\n]+/,
