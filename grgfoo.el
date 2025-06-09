01a0ee51b02823980620fe484097141fc69767d2
andreasjansson <andreas@replicate.ai>
2025-06-10 01:23:56 +0200

    Disable treesit-defun-name-function to debug segfault

--- grgfoo.el
+++ grgfoo.el
@@ -132,7 +132,7 @@
                         "web_search_tool_result" "citations")))
 
     ;; Set up defun name function to show heading type
-    (setq-local treesit-defun-name-function #'grgfoo--defun-name)
+    ;; (setq-local treesit-defun-name-function #'grgfoo--defun-name) ; Disabled to debug segfault
 
     ;; Enable all tree-sitter features
     (treesit-major-mode-setup)))
