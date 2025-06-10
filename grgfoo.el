1743efed04e6c2b783744fe777bafa9277493d69
andreasjansson <andreas@replicate.ai>
2025-06-10 11:44:05 +0200

    Move grgfoo-error-face definition to proper location with other faces

--- grgfoo.el
+++ grgfoo.el
@@ -68,7 +68,10 @@
   "Face for field names like 'Name:', 'ID:', etc."
   :group 'grgfoo)
 
-
+(defface grgfoo-error-face
+  '((t (:background "red" :foreground "white")))
+  "Face for parse errors in grgfoo-mode."
+  :group 'grgfoo)
 
 (defvar grgfoo-mode-syntax-table
   (let ((table (make-syntax-table)))
