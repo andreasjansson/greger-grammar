28b112849f0efeb1ebe6cb1dd0e233f1eea104de
andreasjansson <andreas@replicate.ai>
2025-06-10 20:48:05 +0200

    Remove folding from font-lock feature list

--- grgfoo.el
+++ grgfoo.el
@@ -297,7 +297,7 @@ START and END are the region bounds."
     (setq-local treesit-font-lock-settings grgfoo--treesit-font-lock-settings)
     (setq-local treesit-font-lock-feature-list
                 '((error)
-                  (headers folding)
+                  (headers)
                   (tool-tags comments)
                   (subheadings fields)
                   (tool-tags comments)))
