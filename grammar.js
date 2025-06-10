2ca0a82a5cd95989b19b90e378f723979b811ccf
andreasjansson <andreas@replicate.ai>
2025-06-11 00:42:16 +0200

    Update text rule to handle optional content and prevent trailing newlines

--- grammar.js
+++ grammar.js
@@ -233,15 +233,25 @@ module.exports = grammar({
       $.html_comment,
     ))),
 
-    text: $ => prec.right(seq(
-      $._text_content,
-      repeat(seq(
-        /\n/,
+    text: $ => prec.right(choice(
+      seq(
         $._text_content,
-      ))
+        repeat(seq(
+          /\n/,
+          optional($._text_content),
+        ))
+      ),
+      seq(
+        /\n/,
+        optional($._text_content),
+        repeat(seq(
+          /\n/,
+          optional($._text_content),
+        ))
+      )
     )),
 
-    _text_content: $ => token(prec(-1, /[^`\n]+/)),
+    _text_content: $ => token(prec(-1, /[^`\n<]+/)),
 
     _untagged_text_content: $ => token(prec(-2, seq(/[^#\n]+/, '\n'))),
 
