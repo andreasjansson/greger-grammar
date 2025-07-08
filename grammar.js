7b34fe914c0705ef89b6bbb4b7e92d050a8d179a
andreasjansson <andreas@replicate.ai>
2025-07-08 14:14:50 +0200

    Update text content to exclude ${ pattern but allow standalone $ and {

--- grammar.js
+++ grammar.js
@@ -235,7 +235,7 @@ module.exports = grammar({
       /\n/,
     ))),
 
-    _text_content: $ => token(prec(-1, /[^`<\n]+|<[^/ea-z]/)),
+    _text_content: $ => token(prec(-1, /[^`$<\n]+|<[^/ea-z]|\$[^{]/)),
 
     _assistant_text_content: $ => token(prec(-1, /[^`\n]+/)),
 
