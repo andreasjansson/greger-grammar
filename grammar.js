d1057e891a88d0f5c63d2f774cd512eed077d9b5
andreasjansson <andreas@replicate.ai>
2025-07-08 14:21:40 +0200

    Remove eval braces from externals to let grammar handle them

--- grammar.js
+++ grammar.js
@@ -20,8 +20,6 @@ module.exports = grammar({
     $.tool_content_head,
     $.tool_content_tail,
     $.html_comment,
-    $.eval_start_brace,
-    $.eval_end_brace,
     $.eval_content,
     $.eval_result_start_tag,
     $.eval_result_end_tag,
