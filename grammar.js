3379d1ec24cfc60735b697dfd2c15996e821d227
andreasjansson <andreas@replicate.ai>
2025-07-10 17:53:53 +0200

    Simplify grammar to use code_backticks everywhere

--- grammar.js
+++ grammar.js
@@ -26,7 +26,6 @@ module.exports = grammar({
     $.eval_result_content_head,
     $.eval_result_content_tail,
     $.error_sentinel,
-    $.backtick,
     $.code_backticks,
     $.code_language,
     $.code_contents,
