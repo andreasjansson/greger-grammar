ad0609b705ca67736787ff9ea022fea4ce885794
andreasjansson <andreas@replicate.ai>
2025-07-10 20:00:46 +0200

    Restore optional code_language to grammar

--- grammar.js
+++ grammar.js
@@ -302,6 +302,7 @@ module.exports = grammar({
 
     code: $ => seq(
       $.code_backticks,
+      optional(alias($.code_language_identifier, $.code_language)),
       $.code_contents,
       $.code_backticks,
     ),
