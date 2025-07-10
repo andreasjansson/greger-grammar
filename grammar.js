6e3589a553c4bca1fe939155578bea4de2599eeb
andreasjansson <andreas@replicate.ai>
2025-07-11 01:05:02 +0200

    Make eval_language_spec more specific by using a token instead of bare regex

--- grammar.js
+++ grammar.js
@@ -298,7 +298,7 @@ module.exports = grammar({
 
     eval_end_brace: $ => '}',
 
-    eval_language_spec: $ => /[a-zA-Z0-9_+-]+/,
+    eval_language_spec: $ => token(/[a-zA-Z0-9_+-]+/),
 
     code: $ => seq(
       $.code_backticks,
