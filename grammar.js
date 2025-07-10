4229e450fef604832e5f7ad8a0d024f5808d4a3b
andreasjansson <andreas@replicate.ai>
2025-07-11 01:02:20 +0200

    Rename eval_language to eval_language_spec to avoid conflict with code_language_identifier

--- grammar.js
+++ grammar.js
@@ -293,12 +293,12 @@ module.exports = grammar({
 
     eval_start_brace: $ => seq(
       '${',
-      optional(seq(':', $.eval_language))
+      optional(seq(':', $.eval_language_spec))
     ),
 
     eval_end_brace: $ => '}',
 
-    eval_language: $ => /[a-zA-Z0-9_+-]+/,
+    eval_language_spec: $ => /[a-zA-Z0-9_+-]+/,
 
     code: $ => seq(
       $.code_backticks,
