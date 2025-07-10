314f8dc832763c1089b30df3b865bb5e7074e5fc
andreasjansson <andreas@replicate.ai>
2025-07-10 13:50:06 +0200

    Fix HTML tag parsing by allowing more characters after &lt; in text content

--- grammar.js
+++ grammar.js
@@ -235,7 +235,7 @@ module.exports = grammar({
       /\n/,
     ))),
 
-    _text_content: $ => token(prec(-1, /[^`$<\n]+|<(?!tool\.|eval-result-|!--|\/tool\.|\/eval-result-|safe-shell-commands>)|\$[^{]/)),
+    _text_content: $ => token(prec(-1, /[^`$<\n]+|<[^/!st]|<\/[^et]|<s[^a]|<sa[^f]|<saf[^e]|<safe[^-]|<safe-[^s]|<safe-s[^h]|<t[^o]|<to[^o]|<too[^l]|<tool[^.]|\$[^{]/)),
 
     _assistant_text_content: $ => token(prec(-1, /[^`\n]+/)),
 
