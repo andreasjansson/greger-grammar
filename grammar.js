4ab15cce5c56365bdc784ea52de56330e02e56d3
andreasjansson <andreas@replicate.ai>
2025-07-10 14:14:06 +0200

    Rename external token to avoid conflict

--- grammar.js
+++ grammar.js
@@ -26,7 +26,7 @@ module.exports = grammar({
     $.eval_result_content_head,
     $.eval_result_content_tail,
     $.error_sentinel,
-    $.unclosed_backtick,
+    $.unclosed_backtick_token,
   ],
 
   inline: $ => [
