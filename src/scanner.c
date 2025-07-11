cb4c809b2d9cfdf8cd7a27707a95deec03253108
andreasjansson <andreas@replicate.ai>
2025-07-11 11:42:36 +0200

    Initialize in_code_content state in scanner creation

--- src/scanner.c
+++ src/scanner.c
@@ -160,6 +160,7 @@ void *tree_sitter_greger_external_scanner_create(void) {
     scanner->expecting_eval_result_tail = false;
     scanner->eval_result_id[0] = '\0';
     scanner->fenced_code_block_delimiter_length = 0;
+    scanner->in_code_content = false;
     return scanner;
 }
 
