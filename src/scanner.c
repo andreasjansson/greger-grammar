314a8bff62dc29447d090bc171ca198c003a5a57
andreasjansson <andreas@replicate.ai>
2025-07-11 10:50:33 +0200

    Remove remaining CODE_CONTENTS reference

--- src/scanner.c
+++ src/scanner.c
@@ -895,11 +895,8 @@ bool tree_sitter_greger_external_scanner_scan(void *payload, TSLexer *lexer, con
     }
 
     // Skip whitespace but preserve newlines for other tokens
-    // Don't skip whitespace for code contents as spaces are part of the content
-    if (!valid_symbols[CODE_CONTENTS]) {
-        while (iswspace(lexer->lookahead) && lexer->lookahead != '\n') {
-            skip(lexer);
-        }
+    while (iswspace(lexer->lookahead) && lexer->lookahead != '\n') {
+        skip(lexer);
     }
 
 
