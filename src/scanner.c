59597af1bda500bd54e151faa05fc073def975f5
andreasjansson <andreas@replicate.ai>
2025-07-11 13:48:34 +0200

    Add mark_end at the beginning of scan_code_close_tag like other scanners

--- src/scanner.c
+++ src/scanner.c
@@ -805,6 +805,7 @@ static bool scan_code_end_tag(Scanner *scanner, TSLexer *lexer) {
 
 static bool scan_code_close_tag(Scanner *scanner, TSLexer *lexer) {
     if (lexer->lookahead != '<') return false;
+    lexer->mark_end(lexer);
     advance(lexer);
     
     if (lexer->lookahead != '$') return false;
